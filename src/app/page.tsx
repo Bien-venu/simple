f61e2c601611c1594d759c70139883820a0ae40a
Bien-venu <iambienvenu@gmail.com>
2024-06-26 05:53:59 +0200

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      My app for CMIS
+    </div>
+  )
+}
+
+export default page
